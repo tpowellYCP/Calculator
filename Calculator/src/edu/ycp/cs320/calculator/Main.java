e51ead1c238cec7a779974be3330facf9309e919
David Hovemeyer <dhovemey@ycp.edu>
2013-02-06 09:32:41 -0500

    added another print statement

--- Calculator/src/edu/ycp/cs320/calculator/Main.java
+++ Calculator/src/edu/ycp/cs320/calculator/Main.java
@@ -3,5 +3,6 @@ package edu.ycp.cs320.calculator;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the calculator!");
+		System.out.println("Yeah, this is a great program");
 	}
 }

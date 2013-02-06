90595922d232c693305c69b481b6c1570de8165b
Cody Feltch <cfeltch@ycp.edu>
2013-02-06 09:45:38 -0500

    Merge conflict attempt - added new line

--- Calculator/src/edu/ycp/cs320/calculator/Main.java
+++ Calculator/src/edu/ycp/cs320/calculator/Main.java
@@ -3,6 +3,7 @@ package edu.ycp.cs320.calculator;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the calculator!");
+		System.out.println("This should be a MERGE CONFLICT!!!!");
 		System.out.println("Yeah, this is a great program");
 	}
 }

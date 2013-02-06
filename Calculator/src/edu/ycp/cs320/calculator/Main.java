7ef0c9f3c98e69fb31588a47f1aeecb316ed1dd2
Tom Powell <tpowell@ycp.edu>
2013-02-06 09:50:07 -0500

    Added a new printout statement

--- Calculator/src/edu/ycp/cs320/calculator/Main.java
+++ Calculator/src/edu/ycp/cs320/calculator/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the calculator!");
 		System.out.println("This should be a MERGE CONFLICT!!!!");
+		System.out.println("I forgot!");
 		System.out.println("Yeah, this is a great program");
 	}
 }
